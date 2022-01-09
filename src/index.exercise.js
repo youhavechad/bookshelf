735314da5f923522c1e4515e4af646e3f8984292
Chad Whitt <youhavechad@icloud.com>
2022-01-09 13:52:50 -0600

    WIP.

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -4,23 +4,28 @@ import {Logo} from './components/logo'
 
 function App() {
   return (
-    <>
-      <Logo />
-      <button
-        onClick={() => {
-          alert('Login clicked')
-        }}
-      >
-        Login
-      </button>
-      <button
-        onClick={() => {
-          alert('Regist clicked')
-        }}
-      >
-        Register
-      </button>
-    </>
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button
+          onClick={() => {
+            alert('Login clicked')
+          }}
+        >
+          Login
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            alert('Register clicked')
+          }}
+        >
+          Register
+        </button>
+      </div>
+    </div>
   )
 }
 
